427e49f8e621d7a699d282f7e01cdbfda880011a
Its-KartikSapce <patel0079kartik@gmail.com>
2026-06-16 12:43:53 +0530

    Array Second Program

---
+++ Array/arr2.js
@@ -0,0 +1,7 @@
+// Write a program to find sum of all array elements.			
+var arr =[1,2,3,4,5,6,7,8,9];
+sum=0;
+for(var i=0; i<arr.length;i++){ 
+    sum=sum+arr[i];
+}
+console.log("Sum of Array Elements is",sum)
