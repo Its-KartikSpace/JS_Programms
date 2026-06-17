900b6cdcb20936991af8d415b21285f21ae16e50
Its-KartikSapce <patel0079kartik@gmail.com>
2026-06-17 13:23:35 +0530

    Second  Programm

---
+++ If-else/Ie-2.js
@@ -0,0 +1,10 @@
+
+//  Write a program to check whether a number is negative, positive or zero.
+var a=0;
+if(a>0){
+    console.log(a,"is positive")
+}else if(a<0){
+    console.log(a,"is negative")
+}else{
+    console.log("Number is Zeroo..")
+}
