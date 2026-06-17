92ddd929dd4c7d143beb38bc1e41286f448dbc64
Its-KartikSapce <patel0079kartik@gmail.com>
2026-06-17 13:24:28 +0530

    Second  Programm

---
+++ Loops/L2.js
@@ -0,0 +1,7 @@
+// Write a program to find sum of all natural numbers between 1 to n.		
+var n=100;
+sum=0;
+for(var i=0; i<=n; i++ ){
+    sum=sum+i;
+}
+console.log("Sum of All Natural Number From 1 to n is",sum);
