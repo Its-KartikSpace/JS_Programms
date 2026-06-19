f3a080a45633d2d7d05031ddd7c3e570db7536e3
Its-KartikSapce <patel0079kartik@gmail.com>
2026-06-19 18:17:29 +0530

    Third Programm

---
+++ Loops/L3.js
@@ -0,0 +1,9 @@
+// Write a program to find sum of all even numbers between 1 to n.		
+var n=10;
+sum=0;
+for(var i=0; i<=n; i++){
+    if(i%2==0){
+        sum=sum+i;
+    }
+}
+console.log("Sum of All Even Number From 1 to n is",sum);
