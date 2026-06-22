21b2791ceac7808458c41f4b880eb327fd69c5b6
Its-KartikSapce <patel0079kartik@gmail.com>
2026-06-22 13:42:30 +0530

    Second Program

---
+++ Objects/Obj2.js
@@ -0,0 +1,11 @@
+// Create a car object Print:
+// Brand is Toyota
+// Price is 2000000
+
+var car= {
+    Brand: "Toyoto",
+    Model: "2005",
+    price: 2000000 
+};
+console.log(car['Brand']);
+console.log(car['price']);
