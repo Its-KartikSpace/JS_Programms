25d96edf3fdfc59168aa926f9282721289d82dce
Its-KartikSapce <patel0079kartik@gmail.com>
2026-06-22 15:55:37 +0530

    First Programm

---
+++ For_Each/FE-1.js
@@ -0,0 +1,47 @@
+// 1. Write a program that prints each element of the given array using forEach().
+// let numbers = [5, 10, 15, 20, 25];
+// Output:
+//   5
+//   10
+//   15
+//   20
+//   25
+let arr = [5,10,15,20,25];
+arr.forEach(function (value) {
+    console.log(value);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
