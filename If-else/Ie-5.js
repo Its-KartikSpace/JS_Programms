9bbfc873e67b5331ebfc90cff8ae3e41e0c0bca8
Its-KartikSapce <patel0079kartik@gmail.com>
2026-06-23 13:50:27 +0530

    New Programm

---
+++ If-else/Ie-5.js
@@ -0,0 +1,19 @@
+
+
+// Write a program to input week number and print week day.
+var a=6;
+if(a==1){
+    console.log("Monday")
+}else if(a==2){
+   console.log("Tueday") 
+}else if(a==3){
+   console.log("Wednesday") 
+}else if(a==4){
+   console.log("Thursday") 
+}else if(a==5){
+   console.log("Friday") 
+}else if(a==6){
+   console.log("Saturady") 
+}else{
+   console.log("Sunday")  
+}
